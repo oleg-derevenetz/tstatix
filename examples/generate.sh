8756b6a7f5c485b504259b941f1186bc63057418
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-01-02 16:54:09 +0300

    Add the ability to set variables via command-line.

--- examples/generate.sh
+++ examples/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-../tstatix -encoding utf-8 blog/src       blog/out       *.html *.htm
-../tstatix -encoding utf-8 helloworld/src helloworld/out *.html *.htm
+../tstatix                       -encoding utf-8 blog/src       blog/out       *.html *.htm
+../tstatix -set "content=hello " -encoding utf-8 helloworld/src helloworld/out *.html *.htm
